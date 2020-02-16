2bbb30b74ace7f2433bf23bb4ae162b1d38d1e60
David Moore <dmoore29@ycp.edu>
2020-02-15 22:36:22 -0500

    Added Game.java

---
+++ CS320_Group_Project_Chess/src/edu/ycp/cs320/Group_Project_Chees/model/Game.java
@@ -0,0 +1,30 @@
+package edu.ycp.cs320.Group_Project_Chees.model;
+
+public class Game {
+	
+	private BoardModel board;
+	private Piece[] capturedP1;
+	private Piece[] capturedP2;
+
+
+	
+	// Updates the current state of the board. This function
+	// may not be useful, but its here in case we need it to 
+	// send the board between two players.
+	public void setBoard(BoardModel board) {
+		this.board = board;
+	}
+	
+	public void addCapturedP1() {
+		throw new UnsupportedOperationException("TODO - implement");
+	}
+	
+	public Piece[] getCapturedP1() {
+		throw new UnsupportedOperationException("TODO - implement");
+	}
+	
+	public Piece[] getCapturedP2() {
+		throw new UnsupportedOperationException("TODO - implement");
+	}
+	
+}

31678c01de8954eb30e8e9ae5e2df469f2b78d1e
David Moore <dmoore29@ycp.edu>
2020-02-15 22:20:07 -0500

    Added Piece, Rank, Space

---
+++ CS320_Group_Project_Chess/src/edu/ycp/cs320/Group_Project_Chees/model/Piece.java
@@ -0,0 +1,23 @@
+package edu.ycp.cs320.Group_Project_Chees.model;
+
+public class Piece {
+	private Rank rank;
+	private int player;
+	
+	// Each piece will contain a rank and a player value. Higher 
+	// ranking pieces will be able to over take lower ranking pieces
+	// as long as the piece has a different "player" value.
+	
+	public Piece(Rank rank, int player) {
+		this.rank = rank;
+		this.player = player;
+	}
+	
+	public Rank getRank() {
+		return this.rank;
+	}
+	
+	public int getPlayer() {
+		return this.player;
+	}
+}

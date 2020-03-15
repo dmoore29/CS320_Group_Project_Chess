53d12245c4c340795bf6b92bacb691814355a78f
bnelson24 <bnelson13@ycp.edu>
2020-03-15 15:23:55 -0400

    added basic rook class to test rooktest junit cases
    did not implement the move method yet

---
+++ CS320_Group_Project_Chess/junit/edu/ycp/cs320/Group_Project_Chess_Test/model/RookTest.java
@@ -0,0 +1,39 @@
+package edu.ycp.cs320.Group_Project_Chess_Test.model;
+
+import static org.junit.Assert.*;
+
+import java.awt.Point;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.ycp.cs320.Group_Project_Chess.model.Rook;
+import edu.ycp.cs320.Group_Project_Chess.model.Rank;
+
+
+
+
+
+public class RookTest {
+	private Rook rook;
+	
+	@Before
+	public void SetUp() {
+		rook = new Rook(Rank.ROOK, 0, new Point(4, 0));
+	}
+	
+	@Test
+	public void TestGetRank() {
+		assertEquals(rook.getRank(), Rank.ROOK);
+	}
+	
+	@Test
+	public void TestGetColor() {
+		assertEquals(rook.getColor(), 0);
+	}
+	
+	@Test
+	public void TestGetLocation() {
+		assertEquals(rook.getLocation(), new Point(4, 0));
+	}
+}
---
+++ CS320_Group_Project_Chess/src/edu/ycp/cs320/Group_Project_Chess/model/Rook.java
@@ -0,0 +1,19 @@
+package edu.ycp.cs320.Group_Project_Chess.model;
+
+import java.awt.Point;
+
+public class Rook extends Piece {
+
+	public Rook(Rank rank, int color, Point location) {
+		super(rank, color, location);
+	}
+
+	@Override
+	public void move() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+}

5fec257425e41812f975a872437ce77f9907de0e
Ryan <rblack5@ycp.edu>
2020-03-15 12:18:41 -0400

    Board and Space Models
    - Removed the 'x' and 'y' variables in the 'Space' Class
    		* Added 'startX', 'endX', 'startY', 'endY' variables.
    		This will allow us to determine if the mouse cursor is inside of a specific space.
    
    		* Added 'getArrayLocationX()' and 'getArrayLocationY()' methods to determine location of each space inside the 'Board' array.
    		This value is found by dividing 'StartX' or 'StartY' by 100 (the width of each piece).
    
    	- Added the 'boolean isInside()' method to the 'Space' class.
    	This tells us if the mouse cursor is inside of the space.
    
    	- Modified the 'populateBoard()' method to create spaces with the new variables.

--- CS320_Group_Project_Chess/src/edu/ycp/cs320/Group_Project_Chess/model/Space.java
+++ CS320_Group_Project_Chess/src/edu/ycp/cs320/Group_Project_Chess/model/Space.java
@@ -2,28 +2,64 @@ package edu.ycp.cs320.Group_Project_Chess.model;
 
 public class Space {
 	private Piece piece;
-	private int x, y;
+	private int startX, endX, startY, endY;
 	
 	
 	// Each space contains a single piece, and each piece will 
 	// contain a rank. Spaces that are not filled will be null.
 
-	public Space(Piece piece, int x, int y) {
+	public Space(Piece piece, int startX, int endX, int startY, int endY) {
 		this.piece = piece;
-		this.x = x;
-		this.y =y;
+		this.startX = startX;
+		this.endX = endX;
+		this.startY = startY;
+		this.endY = endY;
 	}
 	
 	public Piece getPiece() {
 		return this.piece;
 	}
+	
 	public void setPiece(Piece temp) {
 		this.piece = temp;
 	}
-	public int getX() {
-		return this.x;
+	
+	public int getStartX() {
+		return this.startX;
 	}
-	public int getY() {
-		return this.y;
+	
+	public int getEndX() {
+		return this.endX;
 	}
+	
+	public int getStartY() {
+		return this.startY;
+	}
+	
+	public int getEndY() {
+		return this.endY;
+	}
+	
+	
+	// Check to see if the mouse cursor is inside of the space.
+	// This will be used to determine the space that the player wants to select.
+	public boolean isInside(int mouseX, int mouseY) {
+		if((this.startX < mouseX && mouseX < this.endX) && (this.startY < mouseY && mouseY < this.endY)) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	// Returns the space's place inside of the 'Board' array.
+	
+	public int getArrayLocationX() {
+		return this.startX / 100;
+	}
+	
+	public int getArrayLocationY() {
+		return this.startY / 100;
+	}
+	
 }

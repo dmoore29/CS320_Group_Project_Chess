e83489a8496fea2c4a766f3c054f3bd016ce5370
bnelson24 <bnelson13@ycp.edu>
2020-03-15 21:58:00 -0400

    added base Game methods and a Player class

--- CS320_Group_Project_Chess/src/edu/ycp/cs320/Group_Project_Chess/model/Game.java
+++ CS320_Group_Project_Chess/src/edu/ycp/cs320/Group_Project_Chess/model/Game.java
@@ -3,33 +3,36 @@ package edu.ycp.cs320.Group_Project_Chess.model;
 public class Game {
 	
 	private Board board;
-	private Piece[] capturedP1;
-	private Piece[] capturedP2;
-
-
+	private Player player1;
+	private Player player2;
 	
-	// Updates the current state of the board. This function
-	// may not be useful, but its here in case we need it to 
-	// send the board between two players.
-	public void setBoard(Board board) {
+	//constructor for a new game of chess
+	public Game(Player player1, Player player2) {
+		board = new Board();
+		this.player1 = player1;
+		this.player2 = player2;
+		
+	}
+	
+	//constructor to load a previous game
+	public Game(Player player1, Player player2, Board board) {
 		this.board = board;
+		this.player1 = player1;
+		this.player2 = player2;
+	}
+	
+	public Player getPlayer1() {
+		return player1;
+	}
+	
+	public Player getPlayer2() {
+		return player2;
 	}
 	
 	public Board getBoard() {
 		return board;
 	}
 	
-	public void addCapturedP1() {
-		throw new UnsupportedOperationException("TODO - implement");
-	}
 	
-	public Piece[] getCapturedP1() {
-		throw new UnsupportedOperationException("TODO - implement");
-	}
-	
-	public Piece[] getCapturedP2() {
-		throw new UnsupportedOperationException("TODO - implement");
-	}
 	
 }
-//changes
---
+++ CS320_Group_Project_Chess/src/edu/ycp/cs320/Group_Project_Chess/model/Player.java
@@ -0,0 +1,13 @@
+package edu.ycp.cs320.Group_Project_Chess.model;
+
+public class Player{
+	private int Color;
+	
+	public Player(int Color) {
+		this.Color = Color;
+	}
+	
+	public int getColor() {
+		return Color;
+	}
+}

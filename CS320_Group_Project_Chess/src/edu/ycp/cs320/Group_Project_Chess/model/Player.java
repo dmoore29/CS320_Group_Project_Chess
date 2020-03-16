7b7d1024ab993203ddc5d36a98bb19689adbc856
bnelson24 <bnelson13@ycp.edu>
2020-03-15 22:04:45 -0400

    added users within Player

--- CS320_Group_Project_Chess/src/edu/ycp/cs320/Group_Project_Chess/model/Player.java
+++ CS320_Group_Project_Chess/src/edu/ycp/cs320/Group_Project_Chess/model/Player.java
@@ -2,12 +2,18 @@ package edu.ycp.cs320.Group_Project_Chess.model;
 
 public class Player{
 	private int Color;
+	private User user;
 	
-	public Player(int Color) {
+	public Player(User user, int Color) {
+		this.user = user;
 		this.Color = Color;
 	}
 	
 	public int getColor() {
 		return Color;
+	}
+	
+	public User getUser() {
+		return user;
 	}
 }

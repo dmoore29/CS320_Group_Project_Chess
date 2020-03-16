6f39b7ec93aa1ab6f4e4f352a1581687973d2fca
David Moore <dmoore29@ycp.edu>
2020-03-16 12:09:29 -0400

    Made changes in chessGameServlet

--- CS320_Group_Project_Chess/src/edu/ycp/cs320/Group_Project_Chess/servlet/ChessGameServlet.java
+++ CS320_Group_Project_Chess/src/edu/ycp/cs320/Group_Project_Chess/servlet/ChessGameServlet.java
@@ -7,8 +7,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import edu.ycp.cs320.Group_Project_Chess.model.Game;
+import edu.ycp.cs320.Group_Project_Chess.controller.GameController;
+import edu.ycp.cs320.Group_Project_Chess.model.Player;
+import edu.ycp.cs320.Group_Project_Chess.model.User;
+
+
 public class ChessGameServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	User u1;
+	User u2;
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
@@ -24,6 +32,13 @@ public class ChessGameServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		System.out.println("ChessGame Servlet: doPost");
+		
+		
+		Player p1 = new Player(u1, 0);
+		Player p2 = new Player(u2, 1);
+		Game model = new Game(p1, p2);
+		GameController controller = new GameController();
+		controller.setModel(model);
 		
 		if (req.getParameter("home") != null) {
 			System.out.println("ChessGame Servlet: forwarding to hHome");

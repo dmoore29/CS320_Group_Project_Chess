f74e2292b3935d7948220b156a4da3cff7ee366b
David Moore <dmoore29@ycp.edu>
2020-03-29 20:51:34 -0400

    Modified validMove functions to recieve board

--- CS320_Group_Project_Chess/src/edu/ycp/cs320/Group_Project_Chess/model/Pawn.java
+++ CS320_Group_Project_Chess/src/edu/ycp/cs320/Group_Project_Chess/model/Pawn.java
@@ -9,7 +9,7 @@ public class Pawn extends Piece {
 	}
 
 	@Override
-	public  Boolean validMove(Point location) {
+	public  Boolean validMove(Point location, Board board) {
 		return false; //FINISH IMPLEMENTING
 	}
 }

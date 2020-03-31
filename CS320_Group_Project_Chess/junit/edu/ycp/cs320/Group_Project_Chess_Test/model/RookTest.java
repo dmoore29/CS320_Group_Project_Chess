3a032cbf9d0ba497a1e965a50f10fba766f01628
Anthony C Beddia <antb41@gmail.com>
2020-03-31 19:54:35 -0400

    rook move test implementation

--- CS320_Group_Project_Chess/junit/edu/ycp/cs320/Group_Project_Chess_Test/model/RookTest.java
+++ CS320_Group_Project_Chess/junit/edu/ycp/cs320/Group_Project_Chess_Test/model/RookTest.java
@@ -64,6 +64,7 @@ public class RookTest {
 		//check move entirety of the board -y 
 		board.setPiece(new Rook(Rank.ROOK, 0, new Point(7, 0))); 
 		assertTrue(board.getPiece(7, 0).validMove(new Point(7, 7), board));
+		board.getSpace(7, 0).setPiece(null);
 		  
 		//check move entirety of the board +x 
 		assertTrue(board.getPiece(0, 7).validMove(new Point(7, 7), board));
@@ -71,6 +72,92 @@ public class RookTest {
 		//check move entirety of the board -x 
 		board.setPiece(new Rook(Rank.ROOK, 0, new Point(7, 4))); 
 		assertTrue(board.getPiece(7, 4).validMove(new Point(0, 4), board));
+		board.getSpace(7, 4).setPiece(null);
+		
+		//check move 1 space forward if piece is in the way +y
+		board.setPiece(new Rook(Rank.ROOK, 0, new Point(0, 6))); 
+		assertFalse(board.getPiece(0, 7).validMove(new Point(0, 6), board));
+		board.getSpace(0, 6).setPiece(null);
+		
+		//check move 1 space backward if piece is in the way -y
+		board.setPiece(new Rook(Rank.ROOK, 0, new Point(4, 4))); 
+		assertFalse(board.getPiece(4, 3).validMove(new Point(4, 4), board));
+		board.getSpace(4, 4).setPiece(null);
+		
+		//check move 1 space horizontally if piece is in the way +x
+		board.setPiece(new Rook(Rank.ROOK, 0, new Point(1, 7))); 
+		assertFalse(board.getPiece(0, 7).validMove(new Point(1, 7), board));
+		board.getSpace(1, 7).setPiece(null);
+		
+		//check move 1 space horizontally if piece is in the way -x
+		board.setPiece(new Rook(Rank.ROOK, 0, new Point(3, 3))); 
+		assertFalse(board.getPiece(4, 3).validMove(new Point(3, 3), board));
+		board.getSpace(3, 3).setPiece(null);
+		
+		//check move 2 spaces forward if piece is in the way +y
+		board.setPiece(new Rook(Rank.ROOK, 0, new Point(0, 6))); 
+		assertFalse(board.getPiece(0, 7).validMove(new Point(0, 5), board));
+		board.getSpace(0, 6).setPiece(null);
+		
+		//check move 2 spaces backward if piece is in the way -y
+		board.setPiece(new Rook(Rank.ROOK, 0, new Point(4, 4))); 
+		assertFalse(board.getPiece(4, 3).validMove(new Point(4, 5), board));
+		board.getSpace(4, 4).setPiece(null);
+		
+		//check move 2 spaces horizontally if piece is in the way +x
+		board.setPiece(new Rook(Rank.ROOK, 0, new Point(1, 7))); 
+		assertFalse(board.getPiece(0, 7).validMove(new Point(2, 7), board));
+		board.getSpace(1, 7).setPiece(null);
+		
+		//check move 2 spaces horizontally if piece is in the way -x
+		board.setPiece(new Rook(Rank.ROOK, 0, new Point(3, 3))); 
+		assertFalse(board.getPiece(4, 3).validMove(new Point(2, 3), board));
+		board.getSpace(3, 3).setPiece(null);
+		
+		//capture piece +y
+		board.setPiece(new Rook(Rank.ROOK, 1, new Point(0, 6))); 
+		assertTrue(board.getPiece(0, 7).validMove(new Point(0, 6), board));
+		board.getSpace(0, 6).setPiece(null);
+		
+		//capture piece -y
+		board.setPiece(new Rook(Rank.ROOK, 1, new Point(4, 4))); 
+		assertTrue(board.getPiece(4, 3).validMove(new Point(4, 4), board));
+		board.getSpace(4, 4).setPiece(null);
+		
+		//capture piece +x
+		board.setPiece(new Rook(Rank.ROOK, 1, new Point(1, 7))); 
+		assertTrue(board.getPiece(0, 7).validMove(new Point(1, 7), board));
+		board.getSpace(1, 7).setPiece(null);
+		
+		//capture piece -x
+		board.setPiece(new Rook(Rank.ROOK, 1, new Point(3, 3))); 
+		assertTrue(board.getPiece(4, 3).validMove(new Point(3, 3), board));
+		board.getSpace(3, 3).setPiece(null);
+		
+		//capture piece long distance +y
+		board.setPiece(new Rook(Rank.ROOK, 1, new Point(0, 0))); 
+		assertTrue(board.getPiece(0, 7).validMove(new Point(0, 0), board));
+		board.getSpace(0, 0).setPiece(null);
+		
+		//capture piece long distance -y
+		board.setPiece(new Rook(Rank.ROOK, 1, new Point(7, 7))); 
+		board.setPiece(new Rook(Rank.ROOK, 0, new Point(7, 0))); 
+		assertTrue(board.getPiece(7, 0).validMove(new Point(7, 7), board));
+		board.getSpace(7, 7).setPiece(null);
+		board.getSpace(7, 0).setPiece(null);
+		
+		//capture piece long distance +x
+		board.setPiece(new Rook(Rank.ROOK, 1, new Point(7, 7))); 
+		assertTrue(board.getPiece(0, 7).validMove(new Point(7, 7), board));
+		board.getSpace(7, 7).setPiece(null);
+		
+		//capture piece long distance -x
+		board.setPiece(new Rook(Rank.ROOK, 1, new Point(0, 0))); 
+		board.setPiece(new Rook(Rank.ROOK, 0, new Point(7, 0))); 
+		assertTrue(board.getPiece(7, 0).validMove(new Point(0, 0), board));
+		board.getSpace(0, 0).setPiece(null);
+		board.getSpace(7, 0).setPiece(null);
+		
 		 		
 		/////END ROOK/////
 		

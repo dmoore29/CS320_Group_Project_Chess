7e70442a057eaa7535c1e4283d5adb5f4f2b00f0
David Moore <dmoore29@ycp.edu>
2020-03-31 21:12:33 -0400

    Finished queen validMove()

--- CS320_Group_Project_Chess/src/edu/ycp/cs320/Group_Project_Chess/model/Queen.java
+++ CS320_Group_Project_Chess/src/edu/ycp/cs320/Group_Project_Chess/model/Queen.java
@@ -10,15 +10,10 @@ public class Queen extends Piece {
 
 	@Override
 	public  Boolean validMove(Point dest, Board board) {
-		System.err.println("Ayyyy it's red");
-		System.err.println("Ayyyy it's red");
-		System.err.println("Ayyyy it's red");
-		System.err.println("Ayyyy it's red");		
-		System.err.println("Ayyyy it's red");
-		System.err.println("Ayyyy it's red");
 		
 		if(board.getSpace(dest.x, dest.y).getPiece() != null) {
 			if(board.getSpace(dest.x, dest.y).getPiece().getColor() == color) {
+				System.err.println("1");
 				return false;
 			}
 		}
@@ -27,6 +22,7 @@ public class Queen extends Piece {
 			if(location.x < dest.x) { //if there is a piece in between source and destination on x axis
 				for(int i = location.x+1; i< dest.x; i++) {
 					if(board.getSpace(i, dest.y).getPiece() != null) {
+						System.err.println("2");
 						return false;
 					}
 				}
@@ -34,6 +30,7 @@ public class Queen extends Piece {
 			if(location.x > dest.x) { //if there is a piece in between source and destination on x axis
 				for(int i = dest.x+1; i < location.x; i++) {
 					if(board.getSpace(i, dest.y).getPiece() != null) {
+						System.err.println("3");
 						return false;
 					}
 				}
@@ -43,6 +40,7 @@ public class Queen extends Piece {
 			if(location.y < dest.y) { //if there is a piece in between source and destination on y axis
 				for(int i = location.y+1; i< dest.y; i++) {
 					if(board.getSpace(dest.x, i).getPiece() != null) {
+						System.err.println("4");
 						return false;
 					}
 				}
@@ -51,6 +49,7 @@ public class Queen extends Piece {
 			if(location.y > dest.y) { //if there is a piece in between source and destination on y axis
 				for(int i = dest.y+1; i < location.y; i++) {
 					if(board.getSpace(dest.x, i).getPiece() != null) {
+						System.err.println("5");
 						return false;
 					}
 				}
@@ -60,37 +59,42 @@ public class Queen extends Piece {
 		
 		
 		if(Math.abs(location.x - dest.x) != Math.abs(location.y - dest.y)) { //move is diagonal
+			System.err.println("A");
 			return false;
 		}
 		
 		for(int i=1; i< Math.abs(location.x - dest.x); i++) { //in between spaces
+			System.err.println("I: " + i);
 			if(location.x < dest.x
 					&& location.y < dest.y) { //prevents crash
 				if(board.getSpace(location.x+i, location.y+i).getPiece() != null) { //down, right
+					System.err.println("B");
 				return false;
 				}
 			}
 			if(location.x < dest.x
 					&& location.y > dest.y) {
 				if(board.getSpace(location.x+i, location.y-i).getPiece() != null) { //down, left
+					System.err.println("C");
 				return false;
 				}
 			}
 			if(location.x > dest.x
 					&& location.y < dest.y) {
 				if(board.getSpace(location.x-i, location.y+i).getPiece() != null) { //up, right
+					System.err.println("D");
 				return false;
 				}
 			}
 			if(location.x > dest.x
 					&& location.y > dest.y) {
 				if(board.getSpace(location.x-i, location.y-i).getPiece() != null) { //up, left
+					System.err.println("E");
 				return false;
 				}
 			}
-			return true;
 		}
-		
-		return false;
+		System.err.println("F");
+		return true;
 	}
 }

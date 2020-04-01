221912c729198c11d0eaa7bce34542af70fbbed3
Ryan <rblack5@ycp.edu>
2020-03-31 20:39:09 -0400

    Update King.java

--- CS320_Group_Project_Chess/src/edu/ycp/cs320/Group_Project_Chess/model/King.java
+++ CS320_Group_Project_Chess/src/edu/ycp/cs320/Group_Project_Chess/model/King.java
@@ -16,6 +16,8 @@ public class King extends Piece {
 		
 		// These statements are written separately because it is checking for the Y difference 
 		// and THEN checking for the X difference. This can't be done in an OR statement.
+		//
+		// These check the amount of spaces being moved in each direction.
 		
 		if(Math.abs(yDifference) == 1) {
 			return true;
@@ -23,6 +25,8 @@ public class King extends Piece {
 		else if(Math.abs(xDifference) == 1) {
 			return true;
 		}
+		
+		// Check to see if the destination contains a piece of the same color.
 		else if(board.getSpace(dest.x, dest.y).getPiece().getColor() == color) {
 			return false;
 		}

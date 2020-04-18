12fc616b792797b0d083103da8a5ea311a0dfaac
bnelson24 <bnelson13@ycp.edu>
2020-04-18 15:12:42 -0400

    added persistence exception to our project

--- CS320_Group_Project_Chess/src/edu/ycp/cs320/Group_Project_Chess/database/DerbyDatabase.java
+++ CS320_Group_Project_Chess/src/edu/ycp/cs320/Group_Project_Chess/database/DerbyDatabase.java
@@ -12,9 +12,68 @@ import java.util.List;
 import edu.ycp.cs320.Group_Project_Chess.model.Board;
 import edu.ycp.cs320.Group_Project_Chess.model.Game;
 import edu.ycp.cs320.Group_Project_Chess.model.User;
-import edu.ycp.cs320.Group_Project_Chess.database.InitialData;
-import edu.ycp.cs320.Group_Project_Chess.database.DBUtil;
 
 public class DerbyDatabase{
+	
+// from library example
+	static {
+		try {
+			Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+		} catch (Exception e) {
+			throw new IllegalStateException("Could not load Derby driver");
+		}
+	}
+	
+	private interface Transaction<ResultType> {
+		public ResultType execute(Connection conn) throws SQLException;
+	}
+
+	private static final int MAX_ATTEMPTS = 10;
+// from library example
+	
+	// wrapper SQL transaction function that calls actual transaction function (which has retries)
+		public<ResultType> ResultType executeTransaction(Transaction<ResultType> txn) {
+			try {
+				return doExecuteTransaction(txn);
+			} catch (SQLException e) {
+				throw new PersistenceException("Transaction failed", e);
+			}
+		}
+		
+		// SQL transaction function which retries the transaction MAX_ATTEMPTS times before failing
+		public<ResultType> ResultType doExecuteTransaction(Transaction<ResultType> txn) throws SQLException {
+			Connection conn = connect();
+			
+			try {
+				int numAttempts = 0;
+				boolean success = false;
+				ResultType result = null;
+				
+				while (!success && numAttempts < MAX_ATTEMPTS) {
+					try {
+						result = txn.execute(conn);
+						conn.commit();
+						success = true;
+					} catch (SQLException e) {
+						if (e.getSQLState() != null && e.getSQLState().equals("41000")) {
+							// Deadlock: retry (unless max retry count has been reached)
+							numAttempts++;
+						} else {
+							// Some other kind of SQLException
+							throw e;
+						}
+					}
+				}
+				
+				if (!success) {
+					throw new SQLException("Transaction failed (too many retries)");
+				}
+				
+				// Success!
+				return result;
+			} finally {
+				DBUtil.closeQuietly(conn);
+			}
+		}
 	
 }

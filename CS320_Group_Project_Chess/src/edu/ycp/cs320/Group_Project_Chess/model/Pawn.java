8d2ba2d0508f55ab34ade504a52162d20eed5ab0
David Moore <dmoore29@ycp.edu>
2020-04-19 13:31:35 -0400

    Forced pawns to retain x position while moving by two on first move

--- CS320_Group_Project_Chess/src/edu/ycp/cs320/Group_Project_Chess/model/Pawn.java
+++ CS320_Group_Project_Chess/src/edu/ycp/cs320/Group_Project_Chess/model/Pawn.java
@@ -25,12 +25,14 @@ public class Pawn extends Piece {
 				&& board.getSpace((int)dest.getX(), (int)dest.getY()).getPiece() == null 
 				&& dest.getY() == location.getY()+2
 				&& location.getY() == 1
+				&& dest.x == location.x
 				&& board.getSpace((int)dest.getX(), (int)dest.getY()-1).getPiece() == null) {
 			return true;
 		} else if (color == 0 //piece is white, moving 2 up from start
 				&& board.getSpace((int)dest.getX(), (int)dest.getY()).getPiece() == null 
 				&& dest.getY() == location.getY()-2
 				&& location.getY() == 6
+				&& dest.x == location.x
 				&& board.getSpace((int)dest.getX(), (int)dest.getY()+1).getPiece() == null) {
 			return true;			
 		} else if (color == 1 //piece is black, captures piece down 1 and 1 to either side

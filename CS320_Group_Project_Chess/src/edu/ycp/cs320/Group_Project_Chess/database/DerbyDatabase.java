33c96da56f9231c4799a2d80d1baaed9fee7c239
bnelson24 <bnelson13@ycp.edu>
2020-04-20 21:36:51 -0400

    fixed an error in findGameswithUser

--- CS320_Group_Project_Chess/src/edu/ycp/cs320/Group_Project_Chess/database/DerbyDatabase.java
+++ CS320_Group_Project_Chess/src/edu/ycp/cs320/Group_Project_Chess/database/DerbyDatabase.java
@@ -144,6 +144,8 @@ public class DerbyDatabase{
 					
 					ArrayList<Game> result = new ArrayList<Game>();
 					
+					stmt.setInt(1, player1.getUserId());
+					
 					resultSet = stmt.executeQuery();
 					
 					// for testing that a result was returned
